eae8fee014fd45706b0794263ea1899465881f3d
CRISTIAN FABIAN GOMEZ <cristianf.gomez.mt@correo.usa.edu.co>
2021-12-19 17:12:58 -0500

    first commit

---
+++ src/main/java/retosCiclo4/Reto_5/interfaces/CleaningProductInterface.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package retosCiclo4.Reto_5.interfaces;
+
+import java.util.List;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+
+import retosCiclo4.Reto_5.model.CleaningProduct;
+
+/**
+ *
+ * @author USUARIO
+ */
+public interface CleaningProductInterface extends MongoRepository<CleaningProduct, Integer>{
+   public List<CleaningProduct> findByPriceLessThanEqual(double precio);
+    //Reto 5
+
+    @Query("{'description':{'$regex':'?0','$options':'i'}}")
+    public List<CleaningProduct> findByDescriptionLike(String description); 
+}
